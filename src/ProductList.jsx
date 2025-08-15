e3da987a1bf5c038ac3a1265bc8c995b1c012e00
RustamRahimov <rustam.ragimov.743@gmail.com>
2025-08-15 09:53:30 -0400

    Fixed problem with item not showing on the cart(dispatch identation was missing)

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css';
 import CartItem from './CartItem';
-import { addItem, removeItem, updateQuantity } from './CartSlice';
+import { addItem} from './CartSlice';
 import { useDispatch } from 'react-redux';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setaddedToCart] = useState({});
+    const dispatch = useDispatch()
     const plantsArray = [
         {
             category: "Air Purifying Plants",

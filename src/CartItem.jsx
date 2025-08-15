34db63c1cbba5f90183a67963e34ace3ac897c95
RustamRahimov <rustam.ragimov.743@gmail.com>
2025-08-15 14:24:03 -0400

    Added temporal alert for checkout button for future reference

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -16,6 +16,7 @@ const CartItem = ({ onContinueShopping }) => {
     return totalcost;
   };
 
+  
   const handleContinueShopping = (e) => {
     onContinueShopping(e);
   };
@@ -72,7 +73,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
